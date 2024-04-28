0c1a065dbd3df627d3435d14d4fb7c17d07c53f2
artypratham <prathameshdharmadhikari3@gmail.com>
2024-04-29 04:03:06 +0530

    index.js of server changes

--- server/index.js
+++ server/index.js
@@ -7,7 +7,13 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+app.use(cors(
+  {
+    origin: [""],
+    methods: ["GET","POST","PUT"],
+    credentials: true
+  }
+));
 app.use(express.json());
 const PORT = process.env.PORT || 8000;
 const uri = process.env.MONGODB_URI;
