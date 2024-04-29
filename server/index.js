a23d9299636d3048816a02e5816271b2239d82d1
artypratham <prathameshdharmadhikari3@gmail.com>
2024-04-29 12:50:36 +0530

    added frontend url to cors in backend index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 const app = express();
 app.use(cors(
   {
-    origin: [""],
+    origin: ["https://swipe-frontend-six.vercel.app/"],
     methods: ["GET","POST","PUT"],
     credentials: true
   }
